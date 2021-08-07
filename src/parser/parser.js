4c6dea4b8736fad1aacfd6b4106d401b412e03e8
jlrhe <jan@heikel.fi>
2021-08-07 22:59:04 +0300

    cleanup of react app and filereading for parser

---
+++ src/parser/parser.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+if (typeof process.argv[2] !== "undefined") {
+  const file = process.argv[2];
+  const readFile = (path) => {
+    try {
+      const text = fs.readFileSync(path, "utf8");
+      console.log(text);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const rawRules = readFile(file);
+  console.log(rawRules);
+} else {
+  console.log(
+    "Usage: node parser.js file \n",
+    "Please provide the file to be parsed as an argument \n",
+    "The file must be the same or similar enough format as https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt"
+  );
+}
