46f1e343692eab4f59dea17d75e4d89c1b187bea
jlrhe <jan@heikel.fi>
2021-08-08 12:49:14 +0300

    dynamic fetching of rules, component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import parser from "./parser/parser";
 
 const App = () => {
+  const [rules, setRules] = useState("loading rules...");
+  const [parsedRules, setParsedRules] = useState([]);
+  //cors proxy is fine for fetching static data without credentials
+  const [rulesUrl, setRulesUrl] = useState(
+    "https://thingproxy.freeboard.io/fetch/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt"
+  );
+  /* commented out to avoid fetch during development
+  useEffect(() => {
+    fetch(rulesUrl)
+      .then((response) => {
+        return response.text();
+      })
+      .then((textString) => {
+        setRules(textString);
+      });
+  }, [rulesUrl]);
+*/
+  useEffect(() => {
+    setParsedRules(parser(rules));
+  }, [rules]);
   return (
     <div className="App">
       <header className="page-header">MtG Rules</header>
+      <p>{parsedRules.toString()}</p>
     </div>
   );
 };
--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,21 +1,85 @@
-const fs = require("fs");
+const parser = (rules) => {
+  //hard coded for development
+  let parsed = [
+    {
+      id: "1.",
+      title: "Game Concepts",
+      chapters: [
+        {
+          id: "100.",
+          title: "General",
+          rules: [
+            {
+              id: "100.1.",
+              rule: "These Magic rules apply to any Magic...",
+              example: "",
+            },
+            {
+              id: "100.1a",
+              rule: "A two-player game is a game that begins with only two players.",
+              example: "",
+            },
+          ],
+        },
+        {
+          id: "101.",
+          title: "The Magic Golden Rules",
+          rules: [
+            {
+              id: "101.1",
+              rule: "Whenever a card’s text directly contradicts these ...",
+              example: "",
+            },
+            {
+              id: "101.2",
+              rule: "When a rule or effect allows or directs something to...",
+              example: "",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: "2.",
+      title: "Game Concepts",
+      chapters: [
+        {
+          id: "200.",
+          title: "General",
+          rules: [
+            {
+              id: "200.1.",
+              rule: "The parts of a card are name, mana cost, illustration, color indicator...",
+              example: "",
+            },
+            {
+              id: "200.2",
+              rule: "Some parts of a card are also characteristics of the object that...",
+              example: "",
+            },
+          ],
+        },
+        {
+          id: "201.",
+          title: "Name",
+          rules: [
+            {
+              id: "201.1",
+              rule: "The name of a card is printed...",
+              example: "",
+            },
+            {
+              id: "201.2",
+              rule: "A card’s name is always considered to be the English...",
+              example:
+                "Example: when you test an app it ahelps to have example texts",
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  return parsed;
+};
 
-if (typeof process.argv[2] !== "undefined") {
-  const file = process.argv[2];
-  const readFile = (path) => {
-    try {
-      const text = fs.readFileSync(path, "utf8");
-      return text;
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  const rawRules = readFile(file);
-  console.log(rawRules);
-} else {
-  console.log(
-    "Usage: node parser.js file \n",
-    "Please provide the file to be parsed as an argument \n",
-    "The file must be the same or similar enough format as https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt"
-  );
-}
+export default parser;
